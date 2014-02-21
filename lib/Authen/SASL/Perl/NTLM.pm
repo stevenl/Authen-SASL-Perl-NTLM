2eda7295d33f1bdb0d123f31ce77c1ff52e30ae6
Steven Lee <steven.lee@analyticalmodels.com.au>
2014-02-21 20:27:03 +1100

    Initial working version

---
+++ lib/Authen/SASL/Perl/NTLM.pm
@@ -0,0 +1,80 @@
+package Authen::SASL::Perl::NTLM;
+# ABSTRACT: NTLM authentication plugin for Authen::SASL::Perl
+
+use strict;
+use warnings;
+
+use Authen::NTLM ();
+use Carp ();
+use MIME::Base64 ();
+
+use parent qw(Authen::SASL::Perl);
+
+sub mechanism { return 'NTLM' }
+
+sub client_start {
+    my ($self) = @_;
+
+    Authen::NTLM::ntlm_reset();
+    Authen::NTLM::ntlm_host( $self->host );
+    Authen::NTLM::ntlm_user( $self->_call('user') );
+    Authen::NTLM::ntlm_password( $self->_call('pass') );
+
+    return q{};
+}
+
+sub client_step {
+    my ( $self, $string ) = @_;
+    $string = Authen::NTLM::ntlm( MIME::Base64::encode_base64($string) );
+    return MIME::Base64::decode_base64($string);
+}
+
+sub server_start {
+    my ( $self, $response, $user_cb ) = @_;
+    $user_cb ||= sub {};
+
+    Carp::confess 'server_start not implemented';
+}
+
+sub server_step {
+    my ( $self, $response, $user_cb ) = @_;
+    $user_cb ||= sub {};
+
+    Carp::confess 'server_step not implemented';
+}
+
+1;
+
+=head1 SYNOPSIS
+
+    use Authen::SASL qw(Perl);
+
+    $sasl = Authen::SASL->new(
+        mechanism => 'NTLM',
+        host      => $host,
+        callback  => {
+            user => $user,
+            pass => $password,
+        },
+    );
+
+=head1 CALLBACK
+
+The callbacks used are:
+
+=head2 Client
+
+=for :list
+= user
+The username to be used for authentication.
+= pass
+The user's password to be used for authentication.
+
+=for Pod::Coverage
+mechanism
+client_start
+client_step
+server_start
+server_step
+
+=cut
---
+++ t/ntlm.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+use Test::More;
+
+use Authen::SASL qw(Perl);
+use_ok('Authen::SASL::Perl::NTLM');
+
+done_testing;
